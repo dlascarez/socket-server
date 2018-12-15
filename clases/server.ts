470c6d4a7cdfde523831f4e6bbc947b318f7f512
Damian Lascarez Rodriguez <dlascarez@ticoders.com>
2018-12-15 11:39:10 -0600

    Seccion 4

--- clases/server.ts
+++ clases/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from '../global/environment';
 import socketIO from 'socket.io';
 import http from 'http';
+import * as socket from '../sockets/sockets';
 
 export default class Server {
     private static _instance: Server;
@@ -25,7 +26,13 @@ export default class Server {
 
     private escucharSockets() {
         this.io.on('connection', cliente => {
+            console.log('Cliente conectado.');
 
+            // Mensajes
+            socket.message(cliente, this.io);
+
+            // Desconectar
+            socket.desconectar(cliente);
         });
     }
 
